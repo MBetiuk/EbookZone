a861bf4c0d4d4d477cf7384baa3ef667a235b077
MBetiuk <mbetiuk1@gmail.com>
2022-05-19 22:06:17 +0200

    klasa book + klasa user

---
+++ src/main/java/pl/betiuk/ebookzone/EbookZoneApplication.java
@@ -0,0 +1,13 @@
+package pl.betiuk.ebookzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookZoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/betiuk/ebookzone/EbookZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.betiuk.ebookzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
